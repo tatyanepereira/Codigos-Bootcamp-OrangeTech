6b3b942a4ea2cf51113db36271f0174ae48a04f1
Tatiane Pereira <106749928+tatyanepereira@users.noreply.github.com>
2023-01-11 20:57:57 -0300

    Add files via upload

---
+++ ExercicioMesada.java
@@ -0,0 +1,17 @@
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:  
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next"; 
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.   
+
+import java.util.Scanner; 
+    
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int entrada = leitor.nextInt();
+        int mesada = 50;
+        int x = mesada * entrada;
+        System.out.println(x);
+        
+
+    }
+}
---
+++ ExerciciosQtdadeDoces.java
@@ -0,0 +1,19 @@
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:  
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next"; 
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.   
+
+import java.util.Scanner; 
+    
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int doce = leitor.nextInt();
+        int qtdadeDoces = 2 * doce;
+          System.out.println("O cliente obteve " + qtdadeDoces + " doces");
+        
+        
+        
+        // TODO: Crie a condição necessária para que cada cliente saiba quantos doces vai obter de acordo com a entrada
+
+    }
+}
